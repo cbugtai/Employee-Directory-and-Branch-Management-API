0df0fbe3675436e401aecc839a8d5b180ec0403d
rrc-F2024WI-cbugtai <150991072+cbugtai@users.noreply.github.com>
2025-01-28 17:51:35 +0000

    feat: added employee routes

--- src/api/v1/routes/employeeRoutes.ts
+++ src/api/v1/routes/employeeRoutes.ts
@@ -1,13 +1,19 @@
 import express, { Router } from "express";
-import * as itemController from "../controllers/itemController";
+import * as employeeController from "../controllers/employeeController";
 
 const router: Router = express.Router();
 
 // app.use("/api/v1/employees", employeeRoutes);
 
-router.get("/items", itemController.getAllitems);
-router.post("/items", itemController.createItem);
-router.put("/items/:id", itemController.updateItem);
-router.delete("/items/:id", itemController.deleteItem);
+//Create Employee
+router.post("/employees", employeeController.createEmployee)
+//Get All Employees
+router.get("/employees", employeeController.getAllEmployees)
+//Get Employee by ID
+router.get("/employees/:id", employeeController.getEmployee)
+//Update Employee
+router.put("/employees/:id", employeeController.updateEmployee)
+//Delete Employee
+router.delete("/employees/:id", employeeController.deleteEmployee)
 
 export default router;

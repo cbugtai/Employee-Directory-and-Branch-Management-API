45add96c8e67b676dec1b120ebd961f6d855c0a2
rrc-F2024WI-cbugtai <150991072+cbugtai@users.noreply.github.com>
2025-01-29 21:13:50 +0000

    feat: Added getAllEmployees on  employeeController

--- src/api/v1/controllers/employeeController.ts
+++ src/api/v1/controllers/employeeController.ts
@@ -11,3 +11,16 @@ export const createEmployee = (req: Request, res: Response): void => {
         res.status(500).json({ message: "Error Adding Employee" });
     }
 };
+
+//Get All Employees
+export const getAllEmployees = (req: Request, res: Response): void => {
+    try {
+        const employees = employeeService.getAllEmployees();
+        res.status(200).json({ message: "Employees Retrieved", data: employees });
+    } catch (error) {
+        res.status(500).json({ message: "Error Retrieving Employees"});
+    }
+}
+//Get Employee by ID
+//Update Employee
+//Delete Employee

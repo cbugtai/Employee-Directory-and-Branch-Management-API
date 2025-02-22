988c2eb0f6917dc0fff8bd0b62d22fad09e69e54
rrc-F2024WI-cbugtai <150991072+cbugtai@users.noreply.github.com>
2025-02-22 07:01:34 +0000

    updated: logic controller now passes errors to the next middleware

--- src/api/v1/controllers/logicController.ts
+++ src/api/v1/controllers/logicController.ts
@@ -1,39 +1,41 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import * as logicService from "../services/logicService";
 import { Employee } from "../models/employeeModel";
+import { HTTP_STATUS } from "src/constants/httpConstants";
+import { successResponse } from "../models/responseModel";
 
 /** 
  * @description Get All Employees for a Branch
  * @route GET v1/employees/branch/:branchID
+ * @returns {Promise<void>}
  */
-export const getBranchEmployees = (req: Request, res: Response): void => {
+export const getBranchEmployees = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try{
-        const { branchID } = req.params;
-        const result: Employee[] | undefined = logicService.getBranchEmployees(branchID); 
-        if (result){
-            res.status(200).json({ message: `Employees in Branch ID ${branchID} Retrieved`, data: result});
-        } else {
-            res.status(404).json({ message: `Branch ID ${branchID} Not Found`});
-        }
-    } catch {
-        res.status(500).json({ message: `Error Retrieving Employees`});
+        
+        const result: Employee[] | undefined = logicService.getBranchEmployees(req.params.branchID); 
+        
+        res.status(HTTP_STATUS.OK).json(
+            successResponse(result, `Employees in Branch ID ${req.params.branchID} Retrieved`)
+        );
+    } catch (error) {
+        next(error);
     }
 }
 
 /** 
  * @description Get All Employees by Department
  * @route GET v1/employees//department/:department
+ * @returns {Promise<void>}
  */
-export const getDepartmentEmployees = (req: Request, res: Response): void => {
+export const getDepartmentEmployees = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try{
-        const { department } = req.params;
-        const result: Employee[] | undefined = logicService.getDepartmentEmployees(department); 
-        if (result){
-            res.status(200).json({ message: `Employees in ${department} Department Retrieved`, data: result});
-        } else {
-            res.status(404).json({ message: `${department} Department Not Found`});
-        }
-    } catch {
-        res.status(500).json({ message: `Error Retrieving Employees`});
+        
+        const result: Employee[] | undefined = logicService.getDepartmentEmployees(req.params.department); 
+        
+        res.status(HTTP_STATUS.OK).json(
+            successResponse(result, `Employees in ${req.params.department} Department Retrieved`)
+        );
+    } catch (error) {
+        next(error);
     }
 }

805b534b1ad779d4fd413f8b4107a26aa6c2b361
rrc-F2024WI-cbugtai <150991072+cbugtai@users.noreply.github.com>
2025-02-23 10:47:49 +0000

    unittest: Updated branchService test to reflect firestone integration

--- test/branchService.test.ts
+++ test/branchService.test.ts
@@ -1,123 +1,164 @@
-import { Branch } from "../src/api/v1/models/branchModel"
 import * as branchService from "../src/api/v1/services/branchService"
+import * as firebase from "../src/api/v1/repositories/firestoreRepository"
+import { DocumentData, DocumentSnapshot, QueryDocumentSnapshot, QuerySnapshot } from "node_modules/firebase-admin/lib/firestore"
+import { Branch } from "../src/api/v1/models/branchModel"
 
-describe("Branch Service Testing", () => {
-    let mockBranches: Branch[];
+jest.mock("../src/api/v1/repositories/firestoreRepository")
 
-    beforeEach(() => {
-        mockBranches = [
-            {
+describe("Branch Services Test", () => {
+    describe("createBranch Test", () => {
+        beforeEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it("Should return the branch on successful request", async () => {
+            //Arange
+            const mockID: string = "1"
+            const mockData: Partial<Branch> = {
+                name: "Test Branch",
+                address: "Test Address",
+                phone: "123-456-7890"
+            };
+            (firebase.createDocument as jest.Mock).mockResolvedValue(mockID);
+
+            //Act
+            const result: Branch = await branchService.createBranch(mockData);
+
+            // Assert
+            expect(firebase.createDocument).toHaveBeenCalled();
+
+            expect(result).toEqual({
                 id: "1",
+                name: "Test Branch",
+                address: "Test Address",
+                phone: "123-456-7890"
+            });
+        })
+    })
+
+    describe("getAllBranches Test", () => {
+        beforeEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it("should return all branches on successful request", async () => {
+            //Arrange
+            const mockDocs: QueryDocumentSnapshot[] = [
+                {
+                    id: "branch1",
+                    data: () =>
+                    ({
+                        name: "Vancouver Branch",
+                        address: "1300 Burrard St, Vancouver, BC, V6Z 2C7",
+                        phone: "604-456-0022"
+                    } as DocumentData),
+                } as QueryDocumentSnapshot,
+                {
+                    id: "branch2",
+                    data: () =>
+                    ({
+                        name: "Edmonton Branch",
+                        address: "7250 82 Ave NW, Edmonton, AB, T6B 0G4",
+                        phone: "780-468-6800"
+                    } as DocumentData),
+                } as QueryDocumentSnapshot,
+            ];
+
+            const mockSnapshot: QuerySnapshot = {
+                docs: mockDocs,
+            } as QuerySnapshot;
+            (firebase.getDocuments as jest.Mock).mockResolvedValue(mockSnapshot);
+
+            //Act
+            const result: Branch[] = await branchService.getAllBranches();
+
+            // Assert
+            expect(firebase.getDocuments).toHaveBeenCalledWith("branches");
+            expect(firebase.getDocuments).toHaveBeenCalledTimes(1);
+            expect(result).toHaveLength(2);
+
+            expect(result[0]).toEqual({
+                id: "branch1",
                 name: "Vancouver Branch",
                 address: "1300 Burrard St, Vancouver, BC, V6Z 2C7",
                 phone: "604-456-0022"
-            },
-            {
-                id: "2",
+            });
+
+            expect(result[1]).toEqual({
+                id: "branch2",
                 name: "Edmonton Branch",
                 address: "7250 82 Ave NW, Edmonton, AB, T6B 0G4",
                 phone: "780-468-6800"
-            }
-        ]
-    })
-
-    describe("Create Branch Service Test", () => {
-        const newBranchData = {
-            name: "Calgary Branch",
-            address: "123 4th Ave SW, Calgary, AB, T2P 3N4",
-            phone: "403-123-4567"
-        }
-
-        jest.spyOn(branchService, "createBranch").mockImplementation((newBranchData: Omit<Branch, "id">) => {
-            const previousBranchID: string = mockBranches[mockBranches.length -1]?.id || "0";
-            const newBranch: Branch = {
-                id: (Number(previousBranchID) + 1).toString(),
-                ...newBranchData
-            }
-            mockBranches.push(newBranch);
-            return newBranch;
-        })
-
-        it("Should add the new Branch to the existing Branches array", () => {
-            branchService.createBranch(newBranchData);
-
-            expect(mockBranches.length).toBe(3);
-        })
-        it("should return an Branch object with an id property", () => {
-            expect(branchService.createBranch(newBranchData)).toHaveProperty("id");
-        })
-
-        it("Should return the new Branch data", () => {
-            expect(branchService.createBranch(newBranchData)).toStrictEqual({id: "3", ...newBranchData})
+            });
         })
     })
 
-    describe("Get All Branches Service Test", () => {
-        jest.spyOn(branchService, "getAllBranches").mockImplementation(() => {
-            return mockBranches;
-        })
+    describe("getBranch Test", () => {
+        beforeEach(() => {
+            jest.clearAllMocks();
+        });
 
-        it("Should return all Branch records as an array", () => {
-            expect(branchService.getAllBranches().length).toBe(2)
+        it("Should return branch data with valid ID", async () => {
+            //Arrange
+            const mockBranchData: Partial<Branch>= {
+                name: "Main Branch",
+                address: "123 Main Street",
+                phone: "123-456-7890"
+            };
+            const mockSnapshot: Partial<DocumentSnapshot> = {
+                id: "1",
+                data: jest.fn().mockReturnValue(mockBranchData),
+            };
+            (firebase.getDocumentById as jest.Mock).mockResolvedValue(mockSnapshot);
+
+            //Act
+            const result: Branch = await branchService.getBranch("1")
+
+            //Assert
+            expect(firebase.getDocumentById).toHaveBeenCalled()
+            expect(result).toEqual({ id:"1", ...mockBranchData });
         })
     })
 
-    describe("Get Branch By ID Service Test", () => {
-        jest.spyOn(branchService, "getBranch").mockImplementation((id) => {
-            return (mockBranches.find(branch => branch.id === id));
-        })
+    describe("updateBranch Test", () => {
+        beforeEach(() => {
+            jest.clearAllMocks();
+        });
 
-        it("Should return the Branch Data of the given Branch ID", () => {
-            expect(branchService.getBranch("1")).toStrictEqual(mockBranches[0])
-            expect(branchService.getBranch("2")).toStrictEqual(mockBranches[1])
+        it("SHould update branch data with valid ID", async () => {
+            //Arrange
+            const branchId: string = "1";
+            const updatedData: Partial<Branch> = {
+                name: "Updated Branch Name",
+                address: "Updated Address",
+                phone: "987-654-3210"
+            };
+            (firebase.updateDocument as jest.Mock).mockResolvedValue(undefined);
+
+            //Act
+            const result: Branch = await branchService.updateBranch(branchId, updatedData)
+
+            //Assert
+            expect(firebase.updateDocument).toHaveBeenCalled()
+            expect(result).toEqual({ id:branchId, ...updatedData });
         })
     })
 
-    describe("Update Branch Service Test", () => {
-        const updatedData: Partial<Branch> = {
-            address: "Updated Address",
-            phone: "Updated Phone Number"
-        }
+    describe("deleteBranch Test", () => {
+        beforeEach(() => {
+            jest.clearAllMocks();
+        });
+    
+        it("should delete branch with valid ID", async () => {
+            //Arrange
+            const branchId: string = "branch1";
+            (firebase.deleteDocument as jest.Mock).mockResolvedValue(undefined);
+            
+            //Act
+            await branchService.deleteBranch(branchId);
 
-        jest.spyOn(branchService, "updateBranch").mockImplementation((id, updatedData) => {
-            const branch: Branch | undefined = mockBranches.find(branch => branch.id === id);
-            if (typeof branch === "undefined"){
-                throw new Error(`Branch with ID ${id} not found.`)
-            }
-            const safeUpdate: Partial<Branch> = {...updatedData};
-            delete safeUpdate.id;
-            Object.assign(branch, safeUpdate);
-            return branch;
-        })
-
-        it("Should return the updated data of the Branch", () => {
-            expect(branchService.updateBranch("1",updatedData).address).toBe("Updated Address")
-            expect(branchService.updateBranch("1",updatedData).phone).toBe("Updated Phone Number")
-        })
-        it("Should update the branch data on the branch array", () => {
-            branchService.updateBranch("2",updatedData)
-
-            expect(mockBranches[1].address).toBe("Updated Address")
-            expect(mockBranches[1].phone).toBe("Updated Phone Number")
-        })
-    })
-
-    describe("Delete Branch Service Test", () => {
-        jest.spyOn(branchService, "deleteBranch").mockImplementation((id) => {
-            const index: number = mockBranches.findIndex(branch => branch.id == id);
-            if (index !== -1){
-                mockBranches.splice(index, 1);
-                return true;
-            }
-            return false;
-        })
-
-        it("Should return true if valid id is given", () => {
-            expect(branchService.deleteBranch("1")).toBeTruthy()
-        })
-        it("Should return false if invalid id is given", () => {
-            expect(branchService.deleteBranch("16")).toBeFalsy()
-            expect(branchService.deleteBranch("sixteen")).toBeFalsy()
-        })
+            //Assert
+            expect(firebase.deleteDocument).toHaveBeenCalled();
+        });
     })
 })

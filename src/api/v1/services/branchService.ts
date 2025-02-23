5fb2bb912b19098f6e40b6d56c3f6333c7b8ff5e
rrc-F2024WI-cbugtai <150991072+cbugtai@users.noreply.github.com>
2025-02-23 08:38:14 +0000

    feat: Refactored branchService to use Firestore

--- src/api/v1/services/branchService.ts
+++ src/api/v1/services/branchService.ts
@@ -1,87 +1,78 @@
 import { Branch } from "../models/branchModel";
-import branches from "../../../data/branchData";
+import * as firebase from "../repositories/firestoreRepository"
+import { DocumentData } from "node_modules/firebase-admin/lib/firestore";
+import { ServiceError } from "../errors/errors";
 
-//Create Branch
+const COLLECTION: string = "branches";
+
 /**
- * Adds new Branch
- * 
- * @param newBranchData - branch information, must include Branch name, Address and Phone Number 
- * @throws {error} when any of the required fields missing
- * @returns {Branch} The new Branch with generated ID
+ * @description Adds new Branch
+ * @param {Partial<Branch>} newBranchData - branch information, must include Branch name, Address and Phone Number 
+ * @returns {Promise<Branch>} The new Branch with generated ID
  */
-export const createBranch = (newBranchData: Omit<Branch, "id">): Branch => {
-    if (
-        !newBranchData.name     ||
-        !newBranchData.address  ||
-        !newBranchData.phone    
-    ) {
-        throw new Error(
-            "Missing required fields. Required Fields include Name, Address and Phone Number"
-        )
-    }
+export const createBranch = async (newBranchData: Partial<Branch>): Promise<Branch> => {
+    const id: string = await firebase.createDocument(COLLECTION, newBranchData);
 
-    const previousBranchID:string = branches[branches.length -1]?.id || "0";
-
-    const newBranch: Branch = {
-        id:     (Number(previousBranchID) + 1).toString(),
-        ...newBranchData
-    }
-
-    branches.push(newBranch);
-    return newBranch
+    return { id, ...newBranchData } as Branch;
 }
 
-//Get All Branches
-export const getAllBranches = (): Branch[] => {
-    return branches;
-}
-
-//Get Branch by ID
 /**
- * Get Branch by ID
- * 
- * @param id - id of the Branch
- * @returns {Branch} - returns Branch Data if given branch id existsotherwise returns undefined
+ * @description Get All Branches
+ * @returns {promise<Branch[]>} List of all Branches 
  */
-export const getBranch = (id: string): Branch | undefined => {
-    return (branches.find(branch => branch.id === id));
+export const getAllBranches = async (): Promise<Branch[]> => {
+    const snapshot: FirebaseFirestore.QuerySnapshot = await firebase.getDocuments(COLLECTION);
+
+    return snapshot.docs.map((doc) => {
+        const data: FirebaseFirestore.DocumentData = doc.data();
+        return { id: doc.id, ...data } as Branch
+    });
 }
 
-//Update Branch
 /**
- * Updates Existing Branch Data
- * 
- * @param id - id of the branch to be updated
- * @param updatedData - object with updated Branch Data
+ * @description Get Branch by ID
+ * @param {string} id - id of the Branch
+ * @throws {Error} - error when branch id doesnt exist
+ * @returns {Branch} - returns Branch Data of given branch id 
+ */
+export const getBranch = async (id: string): Promise<Branch> => {
+    const snapshot: FirebaseFirestore.DocumentSnapshot = await firebase.getDocumentById(COLLECTION, id);
+
+    const data: DocumentData | undefined = snapshot.data();
+    if (data) {
+        return {
+            id: snapshot.id,
+            name: data.name,
+            address: data.address,
+            phone: data.phone
+        } as Branch
+    } else {
+        throw new ServiceError(
+            `Id not found`,
+            "DOCUMENT_NOT_FOUND",
+            404
+        );
+    }
+}
+
+/**
+ * @description Updates Existing Branch Data
+ * @param {string} id - id of the branch to be updated
+ * @param {Partial<Branch>} updatedData - object with updated Branch Data
  * @throws {Error} - error when branch id doesnt exist
  * @returns {Branch} - the updated Branch Data
  */
-export const updateBranch = (id: string, updatedData: Partial<Branch>): Branch => {
-    const branch:Branch | undefined = branches.find(branch => branch.id === id);
-    
-    if (typeof branch === "undefined"){
-        throw new Error(`Branch with ID ${id} not found.`)
-    }
-
-    const safeUpdate: Partial<Branch> = {...updatedData};
-    delete safeUpdate.id;
-
-    Object.assign(branch, safeUpdate);
-    return branch;
+export const updateBranch = async (id: string, updatedData: Partial<Branch>): Promise<Branch> => {
+    await firebase.updateDocument(COLLECTION, id, updatedData);
+    return { id, ...updatedData } as Branch;
 }
 
-//Delete Branch
 /**
- * Remove a Branch from the branches array 
- * 
- * @param id - id of the Branch to be deleted.
- * @returns {boolean} - returns true if Branch was remove, false if not
+ * @description Delets a Branch
+ * @param {String} id - id of the Branch to be deleted.
+ * @throws {Error} - error when branch id doesnt exist
+ * @returns {Promise<void>}
  */
-export const deleteBranch = (id: string): boolean => {
-    const index: number = branches.findIndex(branch => branch.id == id);
-    if (index !== -1){
-        branches.splice(index, 1);
-        return true;
-    }
-    return false;
+export const deleteBranch = async (id: string): Promise<void> => {
+    await firebase.deleteDocument(COLLECTION, id)
 }
